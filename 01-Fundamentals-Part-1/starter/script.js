168262253566bbc27cf61efd7fd904d929c03c22
Kingsuk Bose <115909624+Kingsuk-03@users.noreply.github.com>
2023-07-09 23:30:45 +0530

    Add files via upload
    Basic concepts about variables srtings, operators, type conversion, boolean logic, etc.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,94 @@
+// let js = 'amazing';
+// //if (js === 'amazing') alert('Javascript is FUN');
+// console.log(40 + 8 + 23);
+
+// let firstName = "Arko";
+// console.log(firstName);
+
+// true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+
+// javascriptIsFun = 'YES!';
+// console.log(javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1990;
+// console.log(year);
+
+// const ageJonas = 2037 - 1998;
+// const ageAli = 2037 - 1990;
+// console.log(ageJonas, ageAli);
+
+// const firstName = 'arko';
+// const lastName = 'bose';
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// console.log(x);
+
+
+// console.log(ageJonas > ageAli);
+
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+// console.log(jonasNew);
+
+
+// const age = 13;
+
+// if (age >= 18) {
+//     console.log(`Can start driving license`);
+// } else {
+//     const yearLeft = 18 - age;
+//     console.log(`Too young, wait for ${yearLeft} years`);
+// }
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// if (BMIMark > BMIJohn) {
+//     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// } else {
+//     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+//five falsy values 0, ' ', undefined, null, NaN
+// console.log(Boolean(0));
+
+// const money = 0;
+// if (money) {
+//     console.log("Don't spend it all");
+// } else {
+//     console.log("You should get job");
+// }
+
+// let height;
+// if (height) {
+//     console.log('YAY! Height is defined');
+// } else {
+//     console.log('Height is UNDEFINED');
+// }
+
+
+
+
+
+
+
